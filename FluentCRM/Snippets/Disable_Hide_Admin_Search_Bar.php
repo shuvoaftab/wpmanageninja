f00985b217f510df100ab01c5884d9767a223a0f
Ibrahim Sharif <shuvoaftab@gmail.com>
2023-07-22 15:59:46 +0600

    new

---
+++ FluentCRM/Snippets/Disable_Hide_Admin_Search_Bar.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @author Ibrahim Sharif <shuvoaftabibrahim@gmail.com>
+ * Disable or Hide FluentCRM Admin Search Bar
+ * 
+ */
+
+add_filter('fluent_crm/disable_global_search', '__return_true');
