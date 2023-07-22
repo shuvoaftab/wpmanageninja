8f224b1a0bfc2f07dfbc556d64318348be9285da
Ibrahim Sharif <shuvoaftab@gmail.com>
2023-07-22 15:25:38 +0600

    new

---
+++ FluentCRM/Snippets/Add_Contact_Statuses.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Add new Contact Status in FluentCRM. This will not let you send campaigns or emails to any contacts for these statuses.
+ * fluent-crm/app/Functions/helpers.php:fluentcrm_subscriber_statuses()
+ */
+
+function newContactStatus($allStatus){
+    
+    $allStatus['test'] = __('Test', 'fluent-crm');
+    $allStatus['failed'] = __('Failed', 'fluent-crm');
+    
+    return $allStatus;
+}
+
+add_filter('fluent_crm/contact_statuses', 'newContactStatus');
